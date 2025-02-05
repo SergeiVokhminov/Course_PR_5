0c893baa19125335dde28469ddbe318d4fb0b267
sergei_vokhminov <rreaper@list.ru>
2025-02-06 01:12:24 +0300

    обновление проекта

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-# from config import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,7 +32,3 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
-
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
