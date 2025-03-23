4411f543bd84f1c692b89f8ceaa5944d2d0d26d8
sergei_vokhminov <rreaper@list.ru>
2025-03-24 00:01:14 +0300

    обновление проекта

--- config/settings.py
+++ config/settings.py
@@ -238,5 +238,11 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://localhost:80",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
